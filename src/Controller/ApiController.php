8f6339c7839b98718d618d976f4fa2e2004a7651
Paul Mareschi <117636519+Caalagan@users.noreply.github.com>
2024-04-05 11:15:28 +0100

    Add API endpoint for editing category

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -56,4 +56,29 @@ class ApiController extends AbstractController
 
         return new JsonResponse(['message' => 'Category created!'], Response::HTTP_CREATED);
     }
+
+    #[Route('/api/category/edit/{id}', name: 'api_category_edit', methods: ['PUT'])]
+    public function edit(EntityManagerInterface $entityManager, SerializerInterface $serializerinterface, Request $request, ValidatorInterface $validator, CategoryRepository $categoryRepository, $id)
+    {
+        $category = $categoryRepository->find($id);
+
+        if (!$category) {
+            return new JsonResponse(['error' => 'Category not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = $request->getContent();
+
+        $serializerinterface->deserialize($data, Category::class, 'json', ['object_to_populate' => $category]);
+
+        $errors = $validator->validate($category);
+
+        if (count($errors) > 0) {
+            $errorString = (string) $errors;
+            return new JsonResponse(['error' => $errorString], Response::HTTP_BAD_REQUEST);
+        }
+
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Category updated!'], Response::HTTP_OK);
+    }
 }
