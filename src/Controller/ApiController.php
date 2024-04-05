0a9d69e77613d9302c3d9e63575fee5d3ab9d546
Paul Mareschi <117636519+Caalagan@users.noreply.github.com>
2024-04-05 10:43:29 +0100

    Add new API endpoint for creating a category

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -8,11 +8,15 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Category;
+use Symfony\Component\Serializer\Serializer;
 
 class ApiController extends AbstractController
 {
     #[Route('/api/category/index', name: 'api_category_index', methods: ['GET'])]
-    public function api(categoryRepository $categoryRepository,  SerializerInterface $serializerinterface, $id): JsonResponse
+    public function api(categoryRepository $categoryRepository,  SerializerInterface $serializerinterface): JsonResponse
     {
         $category = $categoryRepository->findAll();
 
@@ -21,13 +25,29 @@ class ApiController extends AbstractController
         return new JsonResponse($categories);
     }
 
-    #[Route('/api/category/show/{id}', name: 'show', methods: ['GET'])]
+    #[Route('/api/category/show/{id}', name: 'api_category_show', methods: ['GET'])]
     public function category(categoryRepository $categoryRepository,  SerializerInterface $serializerinterface, $id): JsonResponse
     {
+
         $category = $categoryRepository->find($id);
+
+        if (!$category) {
+            return new JsonResponse(['error' => 'Category not found'], Response::HTTP_NOT_FOUND);
+        }
 
         $categories = $serializerinterface->serialize($category, 'json');
 
         return new JsonResponse($categories);
+    }
+
+    #[Route('/api/category/new', name: 'api_category_new', methods: ['POST'])]
+    public function new(EntityManagerInterface $entityManager, SerializerInterface $serializerinterface, Request $request)
+    {
+        $category = $serializerinterface->deserialize($request->getContent(), Category::class, 'json');
+
+        $entityManager->persist($category);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'Category created!'], Response::HTTP_CREATED);
     }
 }
